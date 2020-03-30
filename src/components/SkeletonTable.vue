4f8a1c2aa4b9f00b41e6e3bef2b7f78350e4c9c9
bls20AWS <benjasl@a483e7468bf4.ant.amazon.com>
2020-03-30 22:01:02 +0100

    workign with Lambda custom Auth

---
+++ src/components/SkeletonTable.vue
@@ -0,0 +1,24 @@
+<script>
+    import VueContentLoading from 'vue-content-loading';
+
+    export default {
+      components: {
+        VueContentLoading,
+      },
+    };
+  </script>
+
+  <template>
+    <vue-content-loading :width="700" :height="100">
+     
+     <rect x="0" y="0" rx="4" ry="4" width="300" height="10" />
+     <rect x="520" y="0" rx="4" ry="4" width="50" height="10" />
+
+     <rect x="0" y="30" rx="4" ry="4" width="300" height="10" />
+     <rect x="520" y="30" rx="4" ry="4" width="50" height="10" />
+
+     <rect x="0" y="60" rx="4" ry="4" width="300" height="10" />
+     <rect x="520" y="60" rx="4" ry="4" width="50" height="10" />
+
+    </vue-content-loading>
+  </template>
