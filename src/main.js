15bf1833eba624f7ea4df035a059b4c5f9d261c3
bls20AWS <benjasl@a483e7468bf4.ant.amazon.com>
2020-04-17 11:37:42 +0100

    main.js updated to allow for auth in .env files as well as json

--- src/main.js
+++ src/main.js
@@ -11,33 +11,48 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-var domain 
-var clientId 
-var audience 
 
-
-if(process.env.VUE_APP_Auth0_Domain){
-     domain = process.env.VUE_APP_Auth0_Domain
-     clientId = process.env.VUE_APP_Auth0_ClientId
-     audience = process.env.VUE_APP_Auth0_Audience
-}
-
-
+import { domain,clientId,audience } from "./auth/auth_config.json";
 require('./assets/css/style.css')
 Vue.config.productionTip = false;
 
-Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
-  audience,
-  onRedirectCallback: appState => {
-    router.push(
-      appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    );
-  }
-});
+var domainE = process.env.VUE_APP_Auth0_Domain
+var clientIdE = process.env.VUE_APP_Auth0_clientId
+var audienceE = process.env.VUE_APP_Auth0_Audience
+if(process.env.VUE_APP_Auth0_Domain){ //This block is used when the Config variables are placed inside a .env file instead of a auth_config.json file
+  Vue.use(Auth0Plugin, {
+    domainE,
+    clientIdE,
+    audienceE,
+    onRedirectCallback: appState => {
+      router.push(
+        appState && appState.targetUrl
+          ? appState.targetUrl
+          : window.location.pathname
+      );
+    }
+  });
+}else{
+  Vue.use(Auth0Plugin, {
+    domain,
+    clientId,
+    audience,
+    onRedirectCallback: appState => {
+      router.push(
+        appState && appState.targetUrl
+          ? appState.targetUrl
+          : window.location.pathname
+      );
+    }
+  });
+
+}
+
+
+
+
+
+
 
 Vue.directive("highlightjs", HighlightJs);
 
